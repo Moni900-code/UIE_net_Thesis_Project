758fae88fe33361c1169ffa0f3b0e5d0d43762dd
Lailatul Husna Monika <u1904009@student.cuet.ac.bd>
2025-06-28 12:12:17 +0600

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -3,11 +3,8 @@ import os
 from PIL import Image
 
 def get_image_list(raw_image_path, clear_image_path=None):
-    """
-    Return list of [raw_image_path, clear_image_path or None, image_file_name]
-    clear_image_path can be None (for test without GT)
-    """
     raw_image_list = sorted(os.listdir(raw_image_path))
+    
     if clear_image_path is not None:
         clear_image_list = sorted(os.listdir(clear_image_path))
         assert len(raw_image_list) == len(clear_image_list), "Number of raw and clear images must be same"
@@ -23,34 +20,39 @@ def get_image_list(raw_image_path, clear_image_path=None):
 
 
 class myDataSet(torch.utils.data.Dataset):
-    def __init__(self, raw_image_path, clear_image_path=None, transform=None, is_train=True):
+    def __init__(self, raw_image_path, clear_image_path=None, transform=None, mode="train"):
+        """
+        mode: 'train', 'val', or 'test'
+        - 'train' => input + GT required
+        - 'val'   => only input image, GT = dummy
+        - 'test'  => input + GT required
+        """
         self.raw_image_path = raw_image_path
         self.clear_image_path = clear_image_path
-        self.is_train = is_train
+        self.mode = mode
         self.transform = transform
         self.image_list = get_image_list(self.raw_image_path, self.clear_image_path)
 
     def __getitem__(self, index):
         raw_path, clear_path, image_name = self.image_list[index]
 
+        # Load and transform input image
         raw_img = Image.open(raw_path).convert('RGB')
-        if self.transform is not None:
+        if self.transform:
             raw_img = self.transform(raw_img)
 
-        if clear_path is not None:
+        # Handle GT image
+        if self.mode == "val":
+            # No GT during validation
+            clear_img = torch.zeros_like(raw_img)
+        else:
+            if clear_path is None:
+                raise ValueError(f"GT image not found for mode={self.mode}")
             clear_img = Image.open(clear_path).convert('RGB')
-            if self.transform is not None:
+            if self.transform:
                 clear_img = self.transform(clear_img)
-        else:
-            clear_img = None
 
-        if self.is_train:
-            # Training mode: return both input and GT images
-            return raw_img, clear_img, image_name
-        else:
-            # Testing mode:
-            # return GT if available, else None
-            return raw_img, clear_img, image_name
+        return raw_img, clear_img, image_name
 
     def __len__(self):
         return len(self.image_list)
